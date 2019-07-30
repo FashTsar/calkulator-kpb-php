b66fe5ef33190a2665832b20feaccd79ef184883
FashTsar <fash-rus37@mail.ru>
2019-07-30 17:27:17 +0300

    вынес обработку ошибок из расчётов в файл обработки данных

--- CalculationKPB.php
+++ CalculationKPB.php
@@ -11,59 +11,6 @@ class CalculationKPB
     function calculationBulk ($widthBulk, $heightBulk, $pocketBulk, $fabricWidth, $seam) {
 
         /*
-         * Проверка корректности типов и значений
-        */
-
-        $typeWidthBulk = gettype($widthBulk);
-        $typeHeightBulk = gettype($heightBulk);
-        $typePocketBulk = gettype($pocketBulk);
-        $typeFabricWidth = gettype($fabricWidth);
-        $typeSeam = gettype($seam);
-
-        if ($typeWidthBulk !== "integer" || $widthBulk <= 0) {
-            return "error: incorrect type or value Width Bulk";
-        }
-        if ($typeHeightBulk !== "integer" || $heightBulk <= 0) {
-            return "error: incorrect type or value Height Bulk";
-        }
-        if ($typePocketBulk !== "integer" || $pocketBulk <= 0) {
-            return "error: incorrect type or value Pocket Bulk";
-        }
-        if ($typeFabricWidth !== "integer" || $fabricWidth <= 0) {
-            return "error: incorrect type or value Fabric Width";
-        }
-        if ($typeSeam !== "integer" || $seam <= 0) {
-            return "error: incorrect type or value Seam";
-        }
-        if ($widthBulk > $fabricWidth) {
-            return "error: width Bulk could not be more fabric Width";
-        }
-        if ($widthBulk > $heightBulk) {
-            return "error: width Bulk could not be more height Bulk";
-        }
-        if ($pocketBulk > $fabricWidth) {
-            return "error: pocket Bulk could not be more fabric Width";
-        }
-        if ($seam > $fabricWidth) {
-            return "error: seam could not be more fabric Width";
-        }
-        if ($pocketBulk > $widthBulk) {
-            return "error: pocket Bulk could not be more width Bulk";
-        }
-        if ($pocketBulk > $heightBulk) {
-            return "error: pocket Bulk could not be more height Bulk";
-        }
-        if ($seam > $widthBulk) {
-            return "error: seam could not be more width Bulk";
-        }
-        if ($seam > $heightBulk) {
-            return "error: seam could not be more height Bulk";
-        }
-        if ($seam > $pocketBulk) {
-            return "error: seam could not be more pocket Bulk";
-        }
-
-        /*
          * Определяем остаток от деления ширины ткани, на ширину или на высоту наволочки
          */
 
@@ -97,43 +44,6 @@ class CalculationKPB
     function calculationDuvetCover ($widthDuvetCover, $heightDuvetCover, $fabricWidth, $seam) {
 
         /*
-         * Проверка корректности типов значений
-        */
-
-        $typeWidthDuvetCover = gettype($widthDuvetCover);
-        $typeHeightDuvetCover = gettype($heightDuvetCover);
-        $typeFabricWidth = gettype($fabricWidth);
-        $typeSeam = gettype($seam);
-
-        if ($typeWidthDuvetCover !== "integer" || $widthDuvetCover <= 0) {
-            return "error: incorrect type or value Width Duvet Cover";
-        }
-        if ($typeHeightDuvetCover !== "integer" || $heightDuvetCover <= 0) {
-            return "error: incorrect type or value Height Duvet Cover";
-        }
-        if ($typeFabricWidth !== "integer" || $fabricWidth <= 0) {
-            return "error: incorrect type or value Fabric Width";
-        }
-        if ($typeSeam !== "integer" || $seam <= 0) {
-            return "error: incorrect type or value Seam";
-        }
-        if ($widthDuvetCover > $fabricWidth) {
-            return "error: width Duvet Cover could not be more fabric Width";
-        }
-        if ($widthDuvetCover > $heightDuvetCover) {
-            return "error: width Duvet Cover could not be more height Duvet Cover";
-        }
-        if ($seam > $fabricWidth) {
-            return "error: seam could not be more fabric Width";
-        }
-        if ($seam > $widthDuvetCover) {
-            return "error: seam could not be more width Duvet Cover";
-        }
-        if ($seam > $heightDuvetCover) {
-            return "error: seam could not be more height Duvet Cover";
-        }
-
-        /*
         * Определяем остаток от деления ширины ткани, на ширину или на высоту пододеяльника
         */
 
@@ -165,43 +75,6 @@ class CalculationKPB
     }
 
     function calculationSheet ($widthSheet, $heightSheet, $fabricWidth, $seam) {
-
-        /*
-         * Проверка корректности типов значений
-        */
-
-        $typeWidthSheet = gettype($widthSheet);
-        $typeHeightSheet = gettype($heightSheet);
-        $typeFabricWidth = gettype($fabricWidth);
-        $typeSeam = gettype($seam);
-
-        if ($typeWidthSheet !== "integer" || $widthSheet <= 0) {
-            return "error: incorrect type or value Width Sheet";
-        }
-        if ($typeHeightSheet !== "integer" || $heightSheet <= 0) {
-            return "error: incorrect type or value Height Sheet";
-        }
-        if ($typeFabricWidth !== "integer" || $fabricWidth <= 0) {
-            return "error: incorrect type or value Fabric Width";
-        }
-        if ($typeSeam !== "integer" || $seam <= 0) {
-            return "error: incorrect type or value Seam";
-        }
-        if ($widthSheet > $fabricWidth) {
-            return "error: width Sheet could not be more fabric Width";
-        }
-        if ($widthSheet > $heightSheet) {
-            return "error: width Sheet could not be more height Sheet";
-        }
-        if ($seam > $fabricWidth) {
-            return "error: seam could not be more fabric Width";
-        }
-        if ($seam > $widthSheet) {
-            return "error: seam could not be more width Sheet";
-        }
-        if ($seam > $heightSheet) {
-            return "error: seam could not be more height Sheet";
-        }
 
         /*
         * Определяем остаток от деления ширины ткани, на ширину или на высоту простыни
--- dataDistribution.php
+++ dataDistribution.php
@@ -12,13 +12,148 @@ header("Content-Type: text/html; charset=utf-8");
 
 if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"]) === "xmlhttprequest") {
 
-		//$name = htmlspecialchars($_POST["name"]); // Имя
+    $widthBulk = (int) htmlspecialchars($_POST["widthBulk"]);
+    $heightBulk = (int) htmlspecialchars($_POST["heightBulk"]);
+    $pocketBulk = (int) htmlspecialchars($_POST["pocketBulk"]);
+    $widthDuvetCover = (int) htmlspecialchars($_POST["widthDuvetCover"]);
+    $heightDuvetCover = (int) htmlspecialchars($_POST["heightDuvetCover"]);
+    $widthSheet = (int) htmlspecialchars($_POST["widthSheet"]);
+    $heightSheet = (int) htmlspecialchars($_POST["heightSheet"]);
+    $fabricWidth = (int) htmlspecialchars($_POST["fabricWidth"]);
+    $seam = (int) htmlspecialchars($_POST["seam"]);
 
-		echo "Сообщение успешно отправлено!";
+    /*
+    * Проверка корректности типов и значений
+    */
+
+    $noErrors = true;
+
+    // наволочки
+    $typeWidthBulk = gettype($widthBulk);
+    $typeHeightBulk = gettype($heightBulk);
+    $typePocketBulk = gettype($pocketBulk);
+
+    if ($typeWidthBulk !== "integer" || $widthBulk <= 0) {
+        echo "Ошибка: некорректный тип или значение Ширины Наволочки <br />";
+        $noErrors = false;
+    }
+    if ($typeHeightBulk !== "integer" || $heightBulk <= 0) {
+        echo "Ошибка: некорректный тип или значение Длинны Наволочки <br />";
+        $noErrors = false;
+    }
+    if ($typePocketBulk !== "integer" || $pocketBulk < 0) {
+        echo "Ошибка: некорректный тип или значение Кармана Наволочки <br />";
+        $noErrors = false;
+    }
+    if ($widthBulk > $fabricWidth) {
+        echo "Ошибка: Ширина Наволочки не может быть больше Ширины Ткани <br />";
+        $noErrors = false;
+    }
+    if ($widthBulk > $heightBulk) {
+        echo "Ошибка: Ширина Наволочки не может быть больше Длинны наволочки <br />";
+        $noErrors = false;
+    }
+    if ($pocketBulk > $fabricWidth) {
+        echo "Ошибка: Карман Наволочки не может быть больше Ширины Ткани <br />";
+        $noErrors = false;
+    }
+    if ($pocketBulk > $widthBulk) {
+        echo "Ошибка: Карман Наволочки не может быть больше Ширины Наволочки <br />";
+        $noErrors = false;
+    }
+    if ($pocketBulk > $heightBulk) {
+        echo "Ошибка: Карман Наволочки не может быть больше Длинны Наволочки <br />";
+        $noErrors = false;
+    }
+    if ($seam > $widthBulk) {
+        echo "Ошибка: Шов не может быть больше Ширины Наволочки <br />";
+        $noErrors = false;
+    }
+    if ($seam > $heightBulk) {
+        echo "Ошибка: Шов не может быть больше Длинны Наволочки <br />";
+        $noErrors = false;
+    }
+
+    // пододеяльники
+    $typeWidthDuvetCover = gettype($widthDuvetCover);
+    $typeHeightDuvetCover = gettype($heightDuvetCover);
+
+    if ($typeWidthDuvetCover !== "integer" || $widthDuvetCover <= 0) {
+        echo "Ошибка: некорректный тип или значение Ширины Пододеяльника <br />";
+        $noErrors = false;
+    }
+    if ($typeHeightDuvetCover !== "integer" || $heightDuvetCover <= 0) {
+        echo "Ошибка: некорректный тип или значение Длинны Пододеяльника <br />";
+        $noErrors = false;
+    }
+    if ($widthDuvetCover > $fabricWidth) {
+        echo "Ошибка: Ширина Пододеяльника не может быть больше Ширины Ткани <br />";
+        $noErrors = false;
+    }
+    if ($widthDuvetCover > $heightDuvetCover) {
+        echo "Ошибка: Ширина Пододеяльника не может быть больше Длинны Пододеяльника <br />";
+        $noErrors = false;
+    }
+    if ($seam > $widthDuvetCover) {
+        echo "Ошибка: Шов не может быть больше Ширины Пододеяльника <br />";
+        $noErrors = false;
+    }
+    if ($seam > $heightDuvetCover) {
+        echo "Ошибка: Шов не может быть больше Длинны Пододеяльника <br />";
+        $noErrors = false;
+    }
+
+    // простыни
+    $typeWidthSheet = gettype($widthSheet);
+    $typeHeightSheet = gettype($heightSheet);
+
+    if ($typeWidthSheet !== "integer" || $widthSheet <= 0) {
+        echo "Ошибка: некорректный тип или значение Ширины Простыни <br />";
+        $noErrors = false;
+    }
+    if ($typeHeightSheet !== "integer" || $heightSheet <= 0) {
+        echo "Ошибка: некорректный тип или значение Длинны Простыни <br />";
+        $noErrors = false;
+    }
+    if ($widthSheet > $fabricWidth) {
+        echo "Ошибка: Ширина Простыни не может быть больше Ширины Ткани <br />";
+        $noErrors = false;
+    }
+    if ($widthSheet > $heightSheet) {
+        echo "Ошибка: Ширина Пододеяльника не может быть больше Длинны Простыни <br />";
+        $noErrors = false;
+    }
+    if ($seam > $widthSheet) {
+        echo "Ошибка: Шов не может быть больше Ширины Простыни <br />";
+        $noErrors = false;
+    }
+    if ($seam > $heightSheet) {
+        echo "Ошибка: Шов не может быть больше Длинны Простыни <br />";
+        $noErrors = false;
+    }
+
+    // ткань и шов
+    $typeFabricWidth = gettype($fabricWidth);
+    $typeSeam = gettype($seam);
+
+    if ($typeFabricWidth !== "integer" || $fabricWidth <= 0) {
+        echo "Ошибка: некорректный тип или значение Ширины Ткани <br />";
+        $noErrors = false;
+    }
+    if ($typeSeam !== "integer" || $seam <= 0) {
+        echo "Ошибка: некорректный тип или значение Шва <br />";
+        $noErrors = false;
+    }
+    if ($seam > $fabricWidth) {
+        echo "Ошибка: Шов не может быть больше Ширины Ткани <br />";
+        $noErrors = false;
+    }
+
+    if ($noErrors === true) {
+        echo "Ошибок нет";
+    }
 
 	} else {
-
 		die();
-
 	}
 
