332d1b4c1854e310cd47e3356adb5f82e82962d9
FashTsar <fash-rus37@mail.ru>
2019-07-31 18:11:48 +0300

    добавил файл со стилями и первые стили

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
+<link>
     <meta charset="UTF-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script src="getDataFormCalculationKPB.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <title>Калькулятор КПБ</title>
 </head>
 
 <body>
 
-<h1>Калькулятор рассчёта ткани на КПБ от Sliza.ru</h1>
+<h2>Калькулятор рассчёта ткани на КПБ от Sliza.ru</h2>
 
 <form name="Calculator" method="post" action="">
     <h4>Размеры наволочки</h4>
-    <input type="number" id="widthBulk" placeholder="ширина (см.)">
-    <input type="number" id="heightBulk" placeholder="высота (см.)">
-    <input type="number" id="pocketBulk" placeholder="карман наволочки (см.)">
-    <input type="number" id="countBulk" placeholder="количество (шт.)">
+    <input type="number" id="widthBulk" class="receiving_data" placeholder="ширина (см.)">
+    <input type="number" id="heightBulk" class="receiving_data" placeholder="высота (см.)">
+    <input type="number" id="pocketBulk" class="receiving_data" placeholder="карман наволочки (см.)">
+    <input type="number" id="countBulk" class="receiving_data" placeholder="количество (шт.)">
 
     <h4>Размеры пододеяльника</h4>
-    <input type="number" id="widthDuvetCover" placeholder="ширина (см.)">
-    <input type="number" id="heightDuvetCover" placeholder="высота (см.)">
-    <input type="number" id="countDuvetCover" placeholder="количество (шт.)">
+    <input type="number" id="widthDuvetCover" class="receiving_data" placeholder="ширина (см.)">
+    <input type="number" id="heightDuvetCover" class="receiving_data" placeholder="высота (см.)">
+    <input type="number" id="countDuvetCover" class="receiving_data" placeholder="количество (шт.)">
 
     <h4>Размер простыни</h4>
-    <input type="number" id="widthSheet" placeholder="ширина (см.)">
-    <input type="number" id="heightSheet" placeholder="высота (см.)">
-    <input type="number" id="countSheet" placeholder="количество (шт.)">
+    <input type="number" id="widthSheet" class="receiving_data" placeholder="ширина (см.)">
+    <input type="number" id="heightSheet" class="receiving_data" placeholder="высота (см.)">
+    <input type="number" id="countSheet" class="receiving_data" placeholder="количество (шт.)">
 
     <h4>Прочие данные</h4>
-    <input type="number" id="fabricWidth" placeholder="ширина ткани (см.)">
-    <input type="number" id="seam" placeholder="шов изделия (см.)">
+    <input type="number" id="fabricWidth" class="receiving_data" placeholder="ширина ткани (см.)">
+    <input type="number" id="seam" class="receiving_data" placeholder="шов изделия (см.)">
     <br /><br />
 
     <input type="button" id="submit" value="Посчитать">
